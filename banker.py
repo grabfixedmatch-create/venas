6312b6a017ea67fff373f709d555338deb3ed653
grabfixedmatch-create <grabfixedmatch@gmail.com>
2025-09-01 22:22:43 +0200

    Update banker.py

--- banker.py
+++ banker.py
@@ -21,7 +21,7 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.text, "html.parser")
 
-table = soup.find_all("table", class_="table table-striped myTableSmall")[1]
+table = soup.find_all("table")[1]
 
 # Extract all rows
 rows = table.find("tbody").find_all("tr")
