d898d0d7997d5dc8b2c5117f516c316b1d5170e0
grabfixedmatch-create <grabfixedmatch@gmail.com>
2025-09-01 22:24:49 +0200

    Update banker.py

--- banker.py
+++ banker.py
@@ -17,7 +17,13 @@ formatted_date = today.strftime("%A - %d/%m/%Y")
 
 url = "https://bankerpredict.com/banker-of-the-day"
 
-response = requests.get(url)
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                  "AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/120.0.0.0 Safari/537.36"
+}
+
+response = requests.get(url, headers=headers)
 
 soup = BeautifulSoup(response.text, "html.parser")
 
