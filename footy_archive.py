27279e1f93c047f9e8031dffc23e5c96051afe6f
grabfixedmatch-create <grabfixedmatch@gmail.com>
2026-02-06 19:14:24 +0100

    Update footy_archive.py

--- footy_archive.py
+++ footy_archive.py
@@ -1,8 +1,8 @@
 import random
 from datetime import datetime
-# from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 from playwright.sync_api import sync_playwright
-import requests
+# import requests
 from requests.auth import HTTPBasicAuth
 import os
 
